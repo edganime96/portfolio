60b2e19d1f4873b11e3201cc3ecd1914b1e57f8e
edganime96 <edgardojmarcanoc@gmail.com>
2020-09-11 01:41:45 -0500

    mobile optimization 1

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+const burgerButton = document.querySelector('.burger-button');
+// const ipad = window.matchMedia('only screen and (max-width: 767px)');
+
+// ipad.addListener(validation)
+
+// function validation(event) {
+//     if (event.matches) {
+//         burgerButton.addEventListener("click", function() {
+//             document.querySelector('.menu').classList.toggle('is-active');
+//         });
+//     }
+//     // else {
+//         //     burgerButton.removeEventListener("click", function() {
+//             //         document.querySelector('.menu').classList.toggle('is-active');
+//             //     });
+//             // }
+//             console.log(event);
+// }
+        
+        
+burgerButton.addEventListener("click", function() {
+    document.querySelector('.menu').classList.toggle('is-active');
+});
