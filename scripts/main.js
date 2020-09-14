f1c03d8169e669ec6e9a404a097ddbd12a164e30
edganime96 <edgardojmarcanoc@gmail.com>
2020-09-14 05:12:48 -0500

    toggle added to menu links

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,32 @@
 const burgerButton = document.querySelector('.burger-button');
-// const ipad = window.matchMedia('only screen and (max-width: 767px)');
+const menu = document.querySelector('.menu');
+const ipad = window.matchMedia('screen and (max-width: 767px)');
+const widthViewport = document.documentElement.clientWidth;
 
-// ipad.addListener(validation)
+ipad.addListener(validation)
 
-// function validation(event) {
-//     if (event.matches) {
-//         burgerButton.addEventListener("click", function() {
-//             document.querySelector('.menu').classList.toggle('is-active');
-//         });
-//     }
-//     else {
-//             burgerButton.removeEventListener("click", function() {
-//                     document.querySelector('.menu').classList.toggle('is-active');
-//                 });
-//             }
-//             console.log(event);
-// }
+function validation(event) {
+    if (event.matches) {
+        burgerButton.addEventListener("click", hideShow);
+    }
+    else {
+        burgerButton.removeEventListener("click", hideShow);
+        menu.removeEventListener("click", hideShow);
+    }
+}
 
-// validation(ipad);
-    
+if(widthViewport <= 767) {
+    burgerButton.addEventListener('click', hideShow);
+    menu.addEventListener('click', hideShow);
+    //se le agrega el escuchar de eventos para que al hacer click sobre algun link del menu se elimina la clase is-active automaticamente
+}
+
+validation(ipad);
+
+function hideShow() {
+    menu.classList.toggle('is-active');
+}
         
-burgerButton.addEventListener("click", function() {
-    document.querySelector('.menu').classList.toggle('is-active');
-});
+// burgerButton.addEventListener("click", function() {
+//     document.querySelector('.menu').classList.toggle('is-active');
+// });
